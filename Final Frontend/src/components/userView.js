ac5fb4d9188da06c73487dd798055be13ee21f1b
huzi5830 <mhuziy@csumb.edu>
2023-11-30 15:34:21 -0800

    Table updated and add City works

--- Final Frontend/src/components/userView.js
+++ Final Frontend/src/components/userView.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import { useEffect, useState} from 'react';
 import './userView.css';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function UserView() {
   const [userWeather, setUserWeather] = useState(null);
   const [searchedWeather, setSearchWeather] = useState(null);
   const [citiesWeather, setCities] = useState(null);
+  const [useCelsius,setCelsius] = useState(false);
   const token = sessionStorage.getItem("jwt");
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [city, setCityName] = useState('');
+  const [country_code, setCountryCode] = useState('');
+
+  const handleCityNameChange = (event) => {
+    setCityName(event.target.value);
+  };
+
+  const handleCountryCodeChange = (event) => {
+    setCountryCode(event.target.value);
+  };
+  
+    const open = () => {
+      setDialogOpen(true);
+    };
+  
+    const handleCloseDialog = () => {
+      setDialogOpen(false);
+    };
+
+
+  function addCity(){
+    //alert("HERE");
+    console.log(city)
+    console.log(country_code)
+    const requestData = {
+      city: city,
+      country_code: country_code,
+     
+    };
+  
+    fetch('http://localhost:8080/addcity', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : token
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => {
+        if (response.ok) {
+          handleCloseDialog();
+           return response.json();
+        }
+        else{
+        throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+      })
+      .then(Data=> {
+       
+      })
+      .catch(error => {
+        console.error("Error adding city:", error);
+      });
+  }
+
   function getUserWeather(){
    // console.log("INSIDE USERWEATHER");
     console.log("TOKEN: ",token);
@@ -62,12 +125,13 @@ function UserView() {
         });
     }
     
-    function getDefaultCities(){
+    function getWeathers(){
    // console.log("GETTING CITIES");
     fetch('http://localhost:8080/getweathers', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
+        'Authorization' : token
       },
     })
       .then(response => {
@@ -89,7 +153,7 @@ function UserView() {
       });
     }
   useEffect(() => {
-   getDefaultCities();
+   getWeathers();
   }, []);
 
   useEffect(() => {
@@ -109,10 +173,10 @@ function UserView() {
       <button>Settings</button>
     </div>
       {/* Section for viewing current weather */}
-  {userWeather !== null ? (
+      {userWeather !== null ? (
     <section className="current-weather">
     <h2>Weather At {userWeather.cityName}</h2>
-    Temperature: {userWeather.tempC}<br></br>
+    Temperature: {useCelsius ? `${userWeather.tempC}°C` : `${userWeather.tempF}°F`}<br></br>
     Description: {userWeather.desc}<br></br>
     Wind Speed: {userWeather.windSpeed}<br></br>
     {/* Display other current weather details here */}
@@ -122,13 +186,26 @@ function UserView() {
       <section className="search-section">
         <h2>Search Weather</h2>
         <input type="text" id="cityInput" placeholder="Enter City Name"/>
-        <button id="searchButton" onClick={searchCity}> Search</button>
+        <button onClick={searchCity}> Search</button>
       </section>
 
       {/* displaying weather of multiple cities */}
       <section className="cities-weather">
       <div className="header-container">
-        <h2>Weathers around the World</h2> <button>Add City</button></div>
+        <h2>Weathers around the World</h2>  <button onClick={open}> Add City</button></div>
+        <Dialog open={isDialogOpen} onClose={handleCloseDialog}>
+            <DialogTitle style={{ textAlign:'center'}}>Add student</DialogTitle>
+            <DialogContent id="content" >
+            <TextField className="dialog-input" label="Name of city" cityName="name" onChange={handleCityNameChange}  /><br></br> 
+            <TextField className="dialog-input" label="Country Code" countryCode="code" onChange={handleCountryCodeChange}  /><br></br> 
+            </DialogContent>
+
+            <DialogActions>
+              <Button color="secondary" onClick={handleCloseDialog}>Cancel</Button>
+              <Button id = "addStudentBtn"color="secondary" onClick={addCity}>Add City</Button>
+            </DialogActions>
+          </Dialog>      
+
         {citiesWeather !== null ? (
     <table id="citiesTable">
       <thead>
@@ -143,7 +220,9 @@ function UserView() {
         {citiesWeather.map((city, idx) => (
           <tr key={idx}>
             <td>{city.cityName}</td>
-            <td>{city.tempC}°C</td>
+            <td>
+              {useCelsius ? `${city.tempC}°C` : `${city.tempF}°F`}
+            </td>
             <td>{city.desc}</td>
             <td>{city.windSpeed}</td>
           </tr>
