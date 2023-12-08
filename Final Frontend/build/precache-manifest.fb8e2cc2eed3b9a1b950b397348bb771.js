6c9b8941fc6801cfb0d2b1b2061dfb708665197a
Aurelioghs <Aurelioghs@yahoo.com>
2023-12-07 21:36:48 -0800

    added build folder for AWS deployment

---
+++ Final Frontend/build/precache-manifest.fb8e2cc2eed3b9a1b950b397348bb771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7639d77fc24c486ec61971b1330d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96fba78ee30f6423a9f",
+    "url": "/static/css/main.ee49d072.chunk.css"
+  },
+  {
+    "revision": "abbe3dc15d895ccd0c4e",
+    "url": "/static/js/2.be822eea.chunk.js"
+  },
+  {
+    "revision": "83a24a74e919a180b663",
+    "url": "/static/js/3.d6436ba7.chunk.js"
+  },
+  {
+    "revision": "b96fba78ee30f6423a9f",
+    "url": "/static/js/main.9ae88e6b.chunk.js"
+  },
+  {
+    "revision": "8db8f19f44d4794c6ae3",
+    "url": "/static/js/runtime~main.35325196.js"
+  }
+]);
